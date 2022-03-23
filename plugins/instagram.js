4909db199dcb8794460b099d5261effd6f42b71c
Sanuwa <90607090+sanuwaofficial@users.noreply.github.com>
2022-03-23 18:26:08 +0530

    ⛲Aqua 1.0 ʙᴇᴛᴀ💦

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -5,6 +5,7 @@ const Config = require('../config');
 let wk = Config.WORKTYPE == 'public' ? false : true
 const Language = require('../language');
 const Lang = Language.getString('instagram');
+const ig = require('../lib/insta')
 
 
 
@@ -14,28 +15,30 @@ Aqua.addCommand({ pattern: 'ig ?(.*)', fromMe: wk, desc:Lang.INSTA_DESC, deleteC
 
 if (!iglink) return await message.client.sendMessage(message.jid,Lang.N_URL, MessageType.text, { quoted: message.data });
 if (!match[1].includes('instagram.com')) return await message.client.sendMessage(message.jid,Lang.N_URL,MessageType.text, {quoted: message.data});
-await axios.get(`https://sanuw-api.herokuapp.com/docs/download/igpost?url=${match[1]}&apikey=sanuwa`).then(async (response) => {
+await ig.insta(match[1]).then(async (response) => {
     
-if (response.data.result.type == 'image') {	
+if (response.type == 'image') {	
 
 var load= await message.client.sendMessage(message.jid,Lang.D_POST, MessageType.text, { quoted: message.data });
 var up = await message.client.sendMessage(message.jid,Lang.U_POST, MessageType.text, { quoted: message.data });
 await message.client.deleteMessage(message.jid, {id: load.key.id, remoteJid: message.jid, fromMe: true}) ; 
-
-const msg = '┌───[🐋𝙰𝚀𝚄𝙰𝙱𝙾𝚃🐋]\n\n  *📥INSTAGRAM DOWNLODER*\n\n│❤️ʟɪᴋᴇs: ' + response.data.result.likes +  '\n\n│💬 ᴄᴏᴍᴍᴇɴᴛs: '+ response.data.result.comments + '\n\n│ 📤ᴜᴘʟᴏᴀᴅᴇʀ: ' + response.data.result.username +'\n\n│📂 ᴛʏᴘᴇ: '+ response.data.result.type + '\n\n└───────────◉'
-const linkdata = await axios.get(response.data.result.url, { responseType: 'arraybuffer'})
+var msg = ''
+if (Config.DETAILS == 'true') msg = '┌───[🐋𝙰𝚀𝚄𝙰𝙱𝙾𝚃🐋]\n\n  *📥INSTAGRAM DOWNLODER*\n\n│❤️ʟɪᴋᴇs: ' + response.likes +  '\n\n│💬 ᴄᴏᴍᴍᴇɴᴛs: '+ response.comments + '\n\n│ 📤ᴜᴘʟᴏᴀᴅᴇʀ: ' + response.username +'\n\n│📂 ᴛʏᴘᴇ: '+ response.type + '\n\n└───────────◉'
+if (Config.DETAILS == 'false') msg = Config.CAPTION    
+const linkdata = await axios.get(response.url, { responseType: 'arraybuffer'})
 
 await message.sendMessage(Buffer.from(linkdata.data), MessageType.image , { caption: msg , quoted: message.data }) 
 return await message.client.deleteMessage(message.jid, {id: up.key.id, remoteJid: message.jid, fromMe: true})}
     
-    else if (response.data.result.type == 'video') {
+    else if (response.type == 'video') {
 
 var load= await message.client.sendMessage(message.jid,Lang.D_VID, MessageType.text, { quoted: message.data });   
 var up = await message.client.sendMessage(message.jid,Lang.U_VID , MessageType.text, { quoted: message.data });
 await message.client.deleteMessage(message.jid, {id: load.key.id, remoteJid: message.jid, fromMe: true}) ; 
-
-const msg = '┌───[🐋𝙰𝚀𝚄𝙰𝙱𝙾𝚃🐋]\n\n  *📥INSTAGRAM DOWNLODER*\n\n│❤️ʟɪᴋᴇs: ' + response.data.result.likes +  '\n\n│💬 ᴄᴏᴍᴍᴇɴᴛs: '+ response.data.result.comments + '\n\n│ 📤ᴜᴘʟᴏᴀᴅᴇʀ: ' + response.data.result.username +'\n\n│📂 ᴛʏᴘᴇ: '+ response.data.result.type + '\n\n└───────────◉'
-const linkdata = await axios.get(response.data.result.url, { responseType: 'arraybuffer'})
+var msg = ''
+if (Config.DETAILS == 'true') msg = '┌───[🐋𝙰𝚀𝚄𝙰𝙱𝙾𝚃🐋]\n\n  *📥INSTAGRAM DOWNLODER*\n\n│❤️ʟɪᴋᴇs: ' + response.likes +  '\n\n│💬 ᴄᴏᴍᴍᴇɴᴛs: '+ response.comments + '\n\n│ 📤ᴜᴘʟᴏᴀᴅᴇʀ: ' + response.username +'\n\n│📂 ᴛʏᴘᴇ: '+ response.type + '\n\n└───────────◉'
+if (Config.DETAILS == 'false') msg = Config.CAPTION      
+const linkdata = await axios.get(response.url, { responseType: 'arraybuffer'})
 
 await message.sendMessage(Buffer.from(linkdata.data), MessageType.video , { caption: msg , quoted: message.data})
 return await message.client.deleteMessage(message.jid, {id: up.key.id, remoteJid: message.jid, fromMe: true})}
@@ -48,13 +51,13 @@ Aqua.addCommand({ pattern: 'insta ?(.*)', fromMe: wk, desc:Lang.I_DESC, deleteCo
   if (!iglink) return await message.client.sendMessage(message.jid,Lang.N_USER, MessageType.text, { quoted: message.data });
   const user = iglink.replace("https://www.instagram.com/","")
   var load= await message.client.sendMessage(message.jid,Lang.D_USER, MessageType.text, { quoted: message.data });
-  await axios.get(`https://sanuw-api.herokuapp.com/docs/other/igstalk?username=${user}&apikey=sanuwa`).then(async (response) => {
-    if(!response.data.status) {
+  await ig.igstalk(user).then(async (response) => {
+    if(!response.status == 'error') {
     return await message.client.sendMessage(message.jid,Lang.E_USER, MessageType.text, { quoted: message.data });
        }
   else {
-    const msg = '┌───[🐋𝙰𝚀𝚄𝙰𝙱𝙾𝚃🐋]\n\n  *✨ INSTAGRAM STALK*\n\n│👨‍💻ᴜsᴇʀɴᴀᴍᴇ: ' + response.data.result.Username + '\n\n│👨‍🦰ғᴜʟʟɴᴀᴍᴇ: '+ response.data.result.Fullname + '\n\n│ 👥ғᴏʟʟᴏᴡᴇʀs: '+ response.data.result.Followers + '\n\n│👤ғᴏʟʟᴏᴡɪɴɢ: '+ response.data.result.Following + '\n\n│🗳️ᴘᴏsᴛs: ' + response.data.result.Posts +'\n\n│💬ʙɪᴏ: '+ response.data.result.Biography + '\n\n└───────────◉'
-     const photodata = await axios.get(response.data.result.Profile_Pic, { responseType: 'arraybuffer'}); 
+    const msg = '┌───[🐋𝙰𝚀𝚄𝙰𝙱𝙾𝚃🐋]\n\n  *✨ INSTAGRAM STALK*\n\n│👨‍💻ᴜsᴇʀɴᴀᴍᴇ: ' + response.username + '\n\n│👨‍🦰ғᴜʟʟɴᴀᴍᴇ: '+ response.full_name + '\n\n│ 👥ғᴏʟʟᴏᴡᴇʀs: '+ response.edge_followed_by.count + '\n\n│👤ғᴏʟʟᴏᴡɪɴɢ: '+ response.edge_follow.count + '\n\n│🗳️ᴘᴏsᴛs: ' + response.edge_owner_to_timeline_media.count +'\n\n│💬ʙɪᴏ: '+ response.biography + '\n\n└───────────◉'
+     const photodata = await axios.get(response.profile_pic_url_hd, { responseType: 'arraybuffer'}); 
     await message.sendMessage(Buffer.from(photodata.data), MessageType.image, {mimetype: Mimetype.jpg, caption: msg, quoted: message.data}); 
     await message.client.deleteMessage(message.jid, {id: load.key.id, remoteJid: message.jid, fromMe: true}) ;
   }

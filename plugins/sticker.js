67724d618515390b37adf82dc12ac31c71eaf836
Sanuwa <90607090+sanuwaofficial@users.noreply.github.com>
2022-04-28 14:26:30 +0530

    ⛲Aqua 1.0 ʙᴇᴛᴀ💦

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -10,36 +10,9 @@ const Language = require('../language');
 const Lang = Language.getString('sticker');
 const axios = require('axios')
 const cheerio = require('cheerio')
-const FormData = require('form-data')
-async function imgstic(path) {
-	
-		const bodyForm = new FormData();
-		bodyForm.append('new-image', fs.createReadStream(path))
-		const res = await axios(`https://s5.ezgif.com/png-to-webp`,{
-			method: 'POST',
-			data: bodyForm,
-			
-		})
-        const $ = cheerio.load(res.data)
-       
-        const file = $('input[name="file"]').attr('value')
-       
-       
-        const data = {
-          file: file,
-          'convert-to-webp': 'Convert WebP to MP4!',
-        }
-  const res2 = await axios({
-          method: 'post',
-          url: 'https://ezgif.com/png-to-webp/' + data.file,
-          data: new URLSearchParams(Object.entries(data)) 
-         
-         })  
-  const $2 = cheerio.load(res2.data)  
-  const link = $2('div#output > p.outfile > img').attr('src')
-   const output = 'https:' + link
-return output	
-}
+const { webp2mp4 ,  webp2img ,  img2webp , vid2webp  } = require('../lib/ezgif');
+const uploadFile = require('../lib/uploadfile')
+
 
 Aqua.addCommand({pattern: 'sticker$', fromMe: wk, desc: Lang.STICKER_DESC,  deleteCommand: false}, (async (message, match) => {    
 
@@ -54,19 +27,19 @@ Aqua.addCommand({pattern: 'sticker$', fromMe: wk, desc: Lang.STICKER_DESC,  dele
         });
 
         if (message.reply_message.video === false && message.reply_message.image) {
-           const res =  await imgstic(location)
+          await uploadFile(location).then( async (data) => { 
+          const result = data.result.url
+          const res = await img2webp(result)
            const res2 = await axios.get( res, { responseType: 'arraybuffer'})  
-           await message.sendMessage(Buffer.from(res2.data), MessageType.sticker, { quoted: message.data})
+           await message.sendMessage(Buffer.from(res2.data), MessageType.sticker, { quoted: message.data}) })
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 
         }
 
-        ffmpeg(location)
-            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 600x600"])
-            .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
-            .save('sticker.webp')
-            .on('end', async () => {
-                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker, { quoted: message.data });
-            });
+       await uploadFile(location).then( async (data) => { 
+          const result = data.result.url
+          const res = await vid2webp(result)
+           const res2 = await axios.get( res, { responseType: 'arraybuffer'})  
+           await message.sendMessage(Buffer.from(res2.data), MessageType.sticker, { quoted: message.data}) })
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));

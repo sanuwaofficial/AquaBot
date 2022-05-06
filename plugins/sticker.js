52228bc3467de95f60772cd255f77225a10edf3c
Sanuwa <90607090+sanuwaofficial@users.noreply.github.com>
2022-05-06 08:27:48 +0530

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,3 +1,4 @@
+
 const Aqua = require('../events');
 const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
@@ -8,16 +9,11 @@ const Config = require('../config');
 let wk = Config.WORKTYPE == 'public' ? false : true
 const Language = require('../language');
 const Lang = Language.getString('sticker');
-const axios = require('axios')
-const cheerio = require('cheerio')
-const { webp2mp4 ,  webp2img ,  img2webp , vid2webp  } = require('../lib/ezgif');
-const uploadFile = require('../lib/uploadfile')
-
 
 Aqua.addCommand({pattern: 'sticker$', fromMe: wk, desc: Lang.STICKER_DESC,  deleteCommand: false}, (async (message, match) => {    
 
         if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
-        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text, { quoted: message.data});
+        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
         var location = await message.client.downloadAndSaveMediaMessage({
             key: {
                 remoteJid: message.reply_message.jid,
@@ -27,19 +23,23 @@ Aqua.addCommand({pattern: 'sticker$', fromMe: wk, desc: Lang.STICKER_DESC,  dele
         });
 
         if (message.reply_message.video === false && message.reply_message.image) {
-          await uploadFile(location).then( async (data) => { 
-          const result = data.result.url
-          const res = await img2webp(result)
-           const res2 = await axios.get( res, { responseType: 'arraybuffer'})  
-           await message.sendMessage(Buffer.from(res2.data), MessageType.sticker, { quoted: message.data}) })
+            ffmpeg(location)
+                .outputOptions(["-y", "-vcodec libwebp"])
+                .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+                .save('st.webp')
+                .on('end', async () => {
+                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker, { quoted: message.data});
+            });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 
         }
 
-       await uploadFile(location).then( async (data) => { 
-          const result = data.result.url
-          const res = await vid2webp(result)
-           const res2 = await axios.get( res, { responseType: 'arraybuffer'})  
-           await message.sendMessage(Buffer.from(res2.data), MessageType.sticker, { quoted: message.data}) })
+        ffmpeg(location)
+            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 600x600"])
+            .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+            .save('sticker.webp')
+            .on('end', async () => {
+                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker, { quoted: message.data });
+            });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
